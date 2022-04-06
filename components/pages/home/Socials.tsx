f3b57c84b0b99f9f52e962f5c98cf851f7ab2a7b
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-06 13:30:18 +0100

    Update Socials.tsx

--- components/pages/home/Socials.tsx
+++ components/pages/home/Socials.tsx
@@ -45,6 +45,34 @@ function Socials() {
             </svg>
           </a>
         </div>
+        <div className="text-blackish green-400 border-2  border-blackish green-400 w-10 h-10 rounded-full flex items-center justify-center p-1 hover:text-gray-700">
+          <a href={socialdata[3].link} rel="noreferrer" target="_blank">
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            >
+              <path d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z" />
+            </svg>
+          </a>
+        </div>
+        <div className="text-blackish green-400 border-2  border-blackish green-400 w-10 h-10 rounded-full flex items-center justify-center p-1 hover:text-gray-700">
+          <a href={socialdata[4].link} rel="noreferrer" target="_blank">
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            >
+              <path d="M2.846 6.887c.03-.295-.083-.586-.303-.784l-2.24-2.7v-.403h6.958l5.378 11.795 4.728-11.795h6.633v.403l-1.916 1.837c-.165.126-.247.333-.213.538v13.498c-.034.204.048.411.213.537l1.871 1.837v.403h-9.412v-.403l1.939-1.882c.19-.19.19-.246.19-.537v-10.91l-5.389 13.688h-.728l-6.275-13.688v9.174c-.052.385.076.774.347 1.052l2.521 3.058v.404h-7.148v-.404l2.521-3.058c.27-.279.39-.67.325-1.052v-10.608z" />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
