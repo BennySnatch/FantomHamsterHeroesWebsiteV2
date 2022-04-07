f2a19d928c03f723b5293983de27603c27fb86d7
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-07 15:36:41 +0100

    Update Hero.tsx

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -11,18 +11,12 @@ function Hero() {
 
       <div className="flex-col flex  lg:pl-20">
         {/* text-[#1969ff] */}
-        <h2 className="text-3xl lg:text-4xl font-bold text-blackish/80 uppercase mt-8">
-          Fantom
-        </h2>
         <h1 className="text-[30px] lg:text-7xl font-bold  acme lg:text-left text-blackish mb-8">
-          Hamster Heroes
+          Fantom Hamster Heroes
         </h1>
         <span className="text-lg font-semibold text-gray-700 leading-relaxed lg:text-left">
           Fantom Hamster Heroes is a collection of 3333 uniquely and randomly
-          generated NFTs stored on the Fantom Blockchain. They watch apes, dogs,
-          cats, and birds get released out of their cages and felt their
-          cuteness wasn’t appreciated on the blockchain. To find owners to adopt
-          them, they’ve decided… TO GO HAM ON EVERYTHING!
+          generated NFTs stored on Fantom.
         </span>
       </div>
     </div>
