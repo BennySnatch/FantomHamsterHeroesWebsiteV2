1b980e5c2687b2e810b153e7967987ebfdc484db
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-07 15:56:31 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,8 +168,6 @@ const Home: NextPage = () => {
                 </span>
               </Link>
             </div>
-            </div>
-           <div className="flex items-center justify-between lg:justify-end lg:w-full ">
             <div className="px-6 py-3 border-2 uppercase border-blackish rounded-md cursor-pointer mr-4">
               <Link href="/hamsters">
                 <span className="text-gray-800 font-bold text-xl lg:text-2xl">
@@ -177,6 +175,7 @@ const Home: NextPage = () => {
                 </span>
               </Link>
             </div>
+            </div>            
             <div
               className="px-6 py-3 border-2 uppercase border-blackish rounded-md cursor-pointer"
               onClick={() => connectWallet()}
