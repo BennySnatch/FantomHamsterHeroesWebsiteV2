0592aa4b46d4f96fc783a1920ac0cf2818ed4221
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-07 16:14:08 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -180,7 +180,7 @@ const Home: NextPage = () => {
             <div className="px-6 py-3 border-2 uppercase border-blackish rounded-md cursor-pointer mr-4">
               <Link href="/hamsters">
                 <span className="text-gray-800 font-bold text-xl lg:text-2xl">
-                  Roadmap
+                  Roadmap1
                 </span>
               </Link>
             </div>
