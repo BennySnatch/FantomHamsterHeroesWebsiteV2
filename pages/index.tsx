bed7026b25ed31327fbf0aecfd40948810539a55
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-07 16:17:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -159,15 +159,6 @@ const Home: NextPage = () => {
                 alt=""
               />
             </div>
-            <div className="flex items-center justify-between lg:justify-end lg:w-full ">
-            <div className="px-6 py-3 border-2 uppercase border-blackish rounded-md cursor-pointer mr-4">
-              <Link href="/hamsters">
-                <span className="text-gray-800 font-bold text-xl lg:text-2xl">
-                  Home
-                </span>
-              </Link>
-            </div>
-            </div>
           </Link>
           <div className="flex items-center justify-between lg:justify-end lg:w-full ">
             <div className="px-6 py-3 border-2 uppercase border-blackish rounded-md cursor-pointer mr-4">
