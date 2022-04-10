979075e22a5b97c50fe04d9058480e9781eadf65
fantomHAM <91769940+fantomHAM@users.noreply.github.com>
2022-04-10 11:26:45 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,7 +77,6 @@ const Home: NextPage = () => {
             <Socials />
         </nav>
         <Ham />
-        <Mint balance={contextState.balance} />
         <Marquee />
         <Gallery />
         <Roadmap />
